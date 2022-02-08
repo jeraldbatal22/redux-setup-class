876b19cd11a3fa36169b34e5230dc3566e267c6a
Jerald Batal <jerald.batal@bluefletch.com>
2022-02-08 15:50:02 +0800

    fetch data with json placeholder

--- src/stores/BaseStore.js
+++ src/stores/BaseStore.js
@@ -1,7 +1,17 @@
 import * as axios from 'axios';
+// import http from 'http';
+// import https from 'https';
+
+// import { ConfigFile as Config } from '@/Config';
 
 export class BaseStore {
+
     httpClient = axios.create({
+        // baseURL: Config.serverHost,
+        // timeout: Config.requestTimeout,
+        // keepAlive pools and reuses TCP connections, so it's faster
+        // httpAgent: new http.Agent({ keepAlive: true }),
+        // httpsAgent: new https.Agent({ keepAlive: true }),
         headers: {
             'Content-Type': 'application/json'
         }
